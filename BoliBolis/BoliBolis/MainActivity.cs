b2e4e0a8baf300c3432c9d8c3fbba8b515f8d898
LeoNardoz333 <leo.vel333@gmail.com>
2023-11-24 19:38:38 -0600

    owo

--- BoliBolis/BoliBolis/MainActivity.cs
+++ BoliBolis/BoliBolis/MainActivity.cs
@@ -3,12 +3,17 @@ namespace BoliBolis
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        TextView txtnombre, txtpass;
+        ImageView imagen;
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            txtpass = FindViewById<TextView>(Resource.Id.txtContra);
+            txtnombre = FindViewById<TextView>(Resource.Id.txtUsuario);
+            imagen = FindViewById<ImageView>(Resource.Id.imgUsu);
         }
     }
 }
