5036058a4a6151c6561a6b50eed9ab49feff3bed
N0oahs-C0 <n0oahs.5jp@gmail.com>
2023-11-24 20:57:08 -0600

    comunicacion

--- BoliBolis/BoliBolis/MainActivity.cs
+++ BoliBolis/BoliBolis/MainActivity.cs
@@ -1,19 +1,45 @@
+using Android.Content;
+
 namespace BoliBolis
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        TextView txtnombre, txtpass;
-        ImageView imagen;
+        string Usuario, Pass;
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            txtpass = FindViewById<TextView>(Resource.Id.txtContra);
-            txtnombre = FindViewById<TextView>(Resource.Id.txtUsuario);
-            imagen = FindViewById<ImageView>(Resource.Id.imgUsu);
+            var txtnombre = FindViewById<EditText>
+                (Resource.Id.txtUsuario);
+            var txtpass = FindViewById<EditText>
+                (Resource.Id.txtContra);
+            var imagen = FindViewById<ImageView>
+                (Resource.Id.imgUsu);
+            imagen.SetImageResource(Resource.Drawable.Usuario);
+            var btnIngresar = FindViewById<Button>
+                (Resource.Id.btnIngresar);
+            btnIngresar.Click += delegate
+            {
+                Usuario = txtnombre.Text;
+                Pass = txtpass.Text;
+                if (Usuario == "Admin" && Pass == "123")
+                {
+                    Cargar();
+                }
+            };
+        }
+        private void BtnIngresar_Click(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cargar()
+        {
+            Intent Ventana = new Intent(this, typeof(MenuBolis));
+            StartActivity(Ventana);
         }
     }
 }
---
+++ BoliBolis/BoliBolis/Ventas.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoliBolis
+{
+    [Activity(Label = "Ventas")]
+    public class Ventas : Activity
+    {
+        EditText? txtfkbolis, txtcantidad;
+        Button btnguardar;
+        int fkbolis, cantidad;
+        HttpClient client = new HttpClient();
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            txtcantidad = FindViewById<EditText>
+                (Resource.Id.txtCantidad);
+            txtfkbolis = FindViewById<EditText>
+                (Resource.Id.txtFKbolis);
+            var btnGuardar = FindViewById<Button>
+                (Resource.Id.btnGuardar);
+            btnguardar.Click += async delegate
+            {
+                try
+                {
+
+                    fkbolis = int.Parse(txtfkbolis.Text);
+                    cantidad = int.Parse(txtcantidad.Text);
+                    var API = $"http://192.168.215.49:89/Principal/AlmacenarBolis" +
+                    $"costo={fkbolis}" + $"&categoria={cantidad}";
+                    HttpResponseMessage response = client.GetAsync(API).Result; if (response.IsSuccessStatusCode)
+                    {
+                        var resultado = response.Content.ReadAsStringAsync().Result;
+                        Toast.MakeText(this, resultado.ToString(), ToastLength.Long).Show();
+                    }
+                    txtfkbolis.Text = ""; txtcantidad.Text = "";
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Long).Show();
+                    throw;
+                }
+
+            };
+
+        }
+    }
+}
